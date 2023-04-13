8feaf7563ed1b0cab955c8b1acde8eaf34385b75
jyoungGrace <jyjeon.grace@gmail.com>
2023-04-14 01:05:29 +0900

    test

---
+++ tamp/src/main/java/com/codestates/tamp/TampApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.tamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TampApplication.class, args);
+	}
+
+}
